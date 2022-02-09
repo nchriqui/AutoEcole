7cef36392229a073cc464bd7a6aeb0487a9350df
mehdiabrk <79981263+mehdiabrk@users.noreply.github.com>
2022-02-09 19:29:53 +0100

    Revert "Delete GPI_AUTO-ECOLE/src/config directory"
    This reverts commit 67409d346065cfcb6d75c563a66e7128fad0e26a.

---
+++ GPI_AUTO-ECOLE/src/config/GameConfiguration.java
@@ -0,0 +1,16 @@
+package config;
+
+public class GameConfiguration {
+
+	public static final int WINDOW_WIDTH = 1000;
+	public static final int WINDOW_HEIGHT = 480;
+
+	
+	public static final int BLOCK_SIZE = 40;
+
+	public static final int LINE_COUNT = WINDOW_HEIGHT / BLOCK_SIZE;
+	public static final int COLUMN_COUNT = WINDOW_WIDTH / BLOCK_SIZE;
+	
+	public static final int GAME_SPEED = 1;
+
+}
